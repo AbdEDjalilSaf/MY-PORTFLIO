4b49987bc458459cd5993545277fd821b1f9bc40
Abdo saf <noodsaf01@gmail.com>
2023-11-09 12:35:23 +0100

    first version

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+
+  
+
+
+  return (
+    <div>
+<header>
+<a>S</a>
+<ul>
+  <li>Home</li>
+  <li>PROJECTS</li>
+  <li>ABOUT ME</li>
+  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" className='' fill='#ff00000'><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+</ul>
+</header>
+    </div>
+  )
+}
+
+export default App
