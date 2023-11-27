40f7c164537fa8946e605b3d884c7564de7cf0cd
Abdo saf <noodsaf01@gmail.com>
2023-11-27 12:49:13 +0100

    first two version

--- src/Article.jsx
+++ src/Article.jsx
@@ -9,7 +9,28 @@ function Article(){
 
 return (
     <>
-<section className='article'>
+<section className='article '>
+
+<div className="articleDesgin bg-white">
+<h3 className="articleTitle">I build &design stuff</h3>
+<p className="articlePara">
+Open source
+projects, web apps
+and experimentals.</p>
+<button><a className="uppercase">see my work</a></button>
+
+
+</div>
+<div className="articleDesgin bg-white">
+<h3 className="articleTitle">I write,sometimes</h3>
+<p className="articlePara">
+About design,
+frontend dev,
+learning and life.</p>
+<button><a className="uppercase">read my article</a></button>
+
+
+</div>
 
 
     </section>
