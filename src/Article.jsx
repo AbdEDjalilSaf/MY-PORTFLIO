15aed474d8c7506b91a6b9950636ff64bb917b06
Abdo saf <noodsaf01@gmail.com>
2023-11-27 15:06:36 +0100

    first two version

--- src/Article.jsx
+++ src/Article.jsx
@@ -11,23 +11,23 @@ return (
     <>
 <section className='article '>
 
-<div className="articleDesgin bg-white">
-<h3 className="articleTitle">I build &design stuff</h3>
+<div className="articleDesgin bg-white" id="articleOne">
+<h3 className="articleTitle">I build & design stuff</h3>
 <p className="articlePara">
-Open source
-projects, web apps
+Open source<br />
+projects, web apps<br />
 and experimentals.</p>
-<button><a className="uppercase">see my work</a></button>
+<button className="articleButton relative"><a className="articleLink  uppercase flex justify-center gap-2 items-center">see my work<svg xmlns="http://www.w3.org/2000/svg" fill="#503bd5" height="1em" viewBox="0 0 512 512"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"/></svg></a></button>
 
 
 </div>
 <div className="articleDesgin bg-white">
-<h3 className="articleTitle">I write,sometimes</h3>
+<h3 className="articleTitle">I write , sometimes</h3>
 <p className="articlePara">
-About design,
-frontend dev,
+About design,<br />
+frontend dev,<br />
 learning and life.</p>
-<button><a className="uppercase">read my article</a></button>
+<button className="articleButton "><a className="articleLink  uppercase flex justify-center gap-2 items-center">read my article<svg xmlns="http://www.w3.org/2000/svg" fill="#503bd5" height="1em" viewBox="0 0 512 512"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"/></svg></a></button>
 
 
 </div>
