452f6d000d4b24e275ce62d97621406b1be04f2d
Abdo saf <noodsaf01@gmail.com>
2023-11-29 16:58:24 +0100

    first two version

--- src/Article.jsx
+++ src/Article.jsx
@@ -17,7 +17,7 @@ return (
 Open source<br />
 projects, web apps<br />
 and experimentals.</p>
-<button className="articleButton hover:-z-0 relative btn-radial z-10 reveal group"><a className="articleLink z-50 relative  uppercase flex justify-center gap-2 items-center group-hover:text-white group-hover:z-50 " href="https://github.com/AbdEDjalilSaf">see my work<svg xmlns="http://www.w3.org/2000/svg" fill="#503bd5" height="1em" viewBox="0 0 512 512" className="group-hover:fill-white group-hover:z-50"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"/></svg></a></button>
+<button className="articleButton hover:-z-0 relative btn-radial z-10  group"><a className="articleLink z-50 relative  uppercase flex justify-center gap-2 items-center group-hover:text-white group-hover:z-50 " href="https://github.com/AbdEDjalilSaf">see my work<svg xmlns="http://www.w3.org/2000/svg" fill="#503bd5" height="1em" viewBox="0 0 512 512" className="group-hover:fill-white group-hover:z-50"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"/></svg></a></button>
 
 
 </div>
@@ -27,7 +27,7 @@ and experimentals.</p>
 About design,<br />
 frontend dev,<br />
 learning and life.</p>
-<button  className="articleButton hover:-z-0 relative btn-radial z-10 reveal group"><a className="articleLink z-50 relative  uppercase flex justify-center gap-2 items-center group-hover:text-white group-hover:z-50 " href="https://www.linkedin.com/in/abd-el-djalil-saf-076619265/">read my article<svg xmlns="http://www.w3.org/2000/svg" fill="#503bd5" height="1em" viewBox="0 0 512 512" className="group-hover:fill-white group-hover:z-50"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"/></svg></a></button>
+<button  className="articleButton hover:-z-0 relative btn-radial z-10  group"><a className="articleLink z-50 relative  uppercase flex justify-center gap-2 items-center group-hover:text-white group-hover:z-50 " href="https://www.linkedin.com/in/abd-el-djalil-saf-076619265/">read my article<svg xmlns="http://www.w3.org/2000/svg" fill="#503bd5" height="1em" viewBox="0 0 512 512" className="group-hover:fill-white group-hover:z-50"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"/></svg></a></button>
 
 
 </div>
