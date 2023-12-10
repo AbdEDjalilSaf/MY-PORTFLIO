5360022cbe18eb154d6af5c4aceae6ffcb7651a6
Abdo saf <noodsaf01@gmail.com>
2023-12-10 08:47:31 +0100

    first two version

--- src/AboutME.jsx
+++ src/AboutME.jsx
@@ -1,33 +1,33 @@
 import React from "react";
+import './App.css'
 
 
 
 function AboutME(){
 
 
-
-
 return(
 <>
-
-<p className="text-red-500 bg-black">wiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiittttttttttttttttttttttttttt </p>
-
+<div className="w-full h-48 bg-black text-white">
 
 
 
 
+<h2 className="text-white z-20">Why</h2>
 
 
 
 
 
+
+
+
+
+
+
+</div>
 </>
-
 )
-
-
-
-
 
 }
 
